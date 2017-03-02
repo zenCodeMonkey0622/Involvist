ea857fe36789ad7834de9c1ecec2487a7892967a
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-03-02 07:53:29 -0700

    added authentication js file to define the authentication routes via Express/Router.

---
+++ Backend/ClientServices/authentication.js
@@ -0,0 +1,6 @@
+var express = require("express")
+var authRouter = express.Router()
+
+// add an auth route and a token route? do we need both?
+// i guess it depends on what kind of auth method we 
+// are going to use.
