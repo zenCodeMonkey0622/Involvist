b6dcee074211b2ef7ebe77668d3d3df9583339de
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-03-08 06:49:35 -0700

    added MembershipService type.

---
+++ Backend/ClientServices/auth/MembershpService.js
@@ -0,0 +1,11 @@
+// MembershipService.js
+
+module.exports = MembershipService
+
+function MembershipService()
+{
+}
+
+MembershipService.prototype.areUserCredentialsValid = function(userName, password, scope, callback) {
+    // todo
+}
