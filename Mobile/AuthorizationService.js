00029450f7969633125bdb2222c9d6c4648da655
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-03-08 06:54:10 -0700

    added AuthorizationService type.

---
+++ Mobile/AuthorizationService.js
@@ -0,0 +1,23 @@
+// AuthorizationService.js
+
+module.exports = AuthorizationService
+
+function AuthorizationService()
+{
+}
+
+AuthorizationService.prototype.saveAuthorizationCode = function(codeData, callback) {
+    // todo
+}
+
+AuthorizationService.prototype.saveAccessToken = function(tokenData, callback) {
+    // todo
+}
+
+AuthorizationService.prototype.getAuthorizationcode = function(code, callback) {
+    // todo
+}
+
+AuthorizationService.prototype.getAccessToken = function(token, callback) {
+    // todo
+}
