df109acd2a8940bd27f87caf37f74daedda7f24f
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-03-19 22:13:45 -0600

    debug code to run a simple query on the client collection.

--- Backend/ClientServices/auth/ClientService.js
+++ Backend/ClientServices/auth/ClientService.js
@@ -6,15 +6,28 @@ var mongoClient = require('mongodb').MongoClient;
 var constants = require('../constants');
 var client = require('./Client');
 
+// constructor
 function ClientService()
 {
+  // connect to the mongo db
   var mdbUri = 'mongodb://root:g0ld0ntheceiling@' + constants.CLIENTS_DATA_SOURCE;
-  mongoClient.connect(mdbUri, null, function (err, db) {
+  mongoClient.connect(mdbUri, function (err, db) {
     if (err) console.log(err);
-    else console.log('successfully connected to mongodb');
+    else
+    {
+      // debug do a query for a client by ID
+      db.collection(constants.CLIENTS_COLLECTION).findOne({id: constants.DEBUG.AUTH_CLIENT_ID}, function (err, result) {
+        if (err) console.log(err);
+        else console.log(result);
+      });
+    }
   });
 }
 
+/*
+* returns a Client object via callback by performing
+* a mongo query by id
+*/
 ClientService.prototype.getById = function(id, callback) {
 
     // debug:
@@ -23,11 +36,15 @@ ClientService.prototype.getById = function(id, callback) {
                         constants.DEBUG.AUTH_CLIENT_SECRET,
                         ['password']));
 
-    // todo:
+    // TODO:
     // go to the client collection of the mongo instance to get
     // the correct client object (should be a small collection)
 }
 
+/*
+* returns true if a passed in redirect uri is valid for a given
+* client. false otherwise.
+*/
 ClientService.prototype.isValidRedirectUri = function(client, requestedUri) {
     // debug:
     return true;
--- Backend/ClientServices/constants.js
+++ Backend/ClientServices/constants.js
@@ -1,15 +1,13 @@
 module.exports = {
     USER_MANAGEMENT_DATA_SOURCE: 'ds147069.mlab.com:47069/projectwoke',
-    USER_MANAGEMENT_FRAGMENT: '',
+    USER_MANAGEMENT_COLLECTION: '',
     CLIENTS_DATA_SOURCE: 'ds147069.mlab.com:47069/projectwoke',
-    CLIENTS_FRAGMENT: '',
+    CLIENTS_COLLECTION: 'clients',
     AUTH_DATA_SOURCE: '',
-    AUTH_FRAGMENT: '',
-    
+    AUTH_COLLECTION: '',
+
     DEBUG: {
-    AUTH_CLIENT_ID: '1',
+    AUTH_CLIENT_ID: 1,
     AUTH_CLIENT_SECRET: 'fefifofum'
     }
 }
-
-    
