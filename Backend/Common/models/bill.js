410e95730d61a001fe3be5dc6cdd1d9a71cbaf12
zenCodeMonkey0622 <ryan.crowley1976@gmail.com>
2017-04-07 17:18:21 -0500

    Refactored bills query

--- Backend/Common/models/bill.js
+++ Backend/Common/models/bill.js
@@ -5,27 +5,28 @@ var Schema = mongoose.Schema;
 
 //Information from get Bills by congressman and specific details
 var BillSchema = new Schema({
-	 congress: String,
-	 number: String,
-	 bill: String,
-	 bill_uri: String,
-	 title: String,
-	 sponsor: String,
-	 sponsor_id: String,
-	 sponsor_uri: String,
-	 gpo_pdf_uri: String,
-	 introduced_date: String,
-	 cosponsors: String,
-	 primary_subject: String,
-	 committees: String,
-	 latest_major_action_date: String,
-	 latest_major_action: String,
-	 house_passage_vote: String,
-	 senate_passage_vote: String,
-	 versions: [],
-	 actions: [{}],
-	 //Added 
-	 tags: []
+	congress: String,
+	number: String,
+	bill: String,
+	bill_uri: String,
+	title: String,
+	sponsor: String,
+	sponsor_id: String,
+	sponsor_uri: String,
+	gpo_pdf_uri: String,
+	introduced_date: String,
+	cosponsors: String,
+	primary_subject: String,
+	committees: String,
+	latest_major_action_date: String,
+	latest_major_action: String,
+	house_passage_vote: String,
+	senate_passage_vote: String,
+	versions: [],
+	actions: [{}],
+	//Added 
+	tags: [],
+    description: String
 });
 
 var Bill = mongoose.model("Bill", BillSchema);
