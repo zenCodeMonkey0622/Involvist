99e9a67623cab54bc36c8f1a61a0c293a80befd5
zenCodeMonkey0622 <ryan.crowley1976@gmail.com>
2017-04-11 16:07:46 -0500

    Added cluster support.

--- Backend/ClientServices/index.js
+++ Backend/ClientServices/index.js
@@ -1,15 +1,30 @@
 // index.js
 // main entry-point for involvist client services
-
+var cluster = require('cluster');
+var express = require('express');
+var numCPUs = require('os').cpus().length;
 var auth = require('./auth/authentication');
 var gateway = require('./endpoints/gateway');
-var express = require('express');
-var app = express();
 
-app.use('/oauth', auth.AuthenticationRouter);
-app.use('/api', gateway);
+cluster.schedulingPolicy = cluster.SCHED_RR;
 
-// debug
-app.listen('3000', function () {
-    console.log('listening on 3000');
-});
+if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+        // Create a worker
+        cluster.fork();
+    }
+} else {
+    // Workers share the TCP connection in this server
+    var app = express();
+
+    app.use('/oauth', auth.AuthenticationRouter);
+    app.use('/api', gateway);
+
+    // debug
+    app.listen('3000', function () {
+        console.log('listening on 3000');
+    });
+}
+
+
+
