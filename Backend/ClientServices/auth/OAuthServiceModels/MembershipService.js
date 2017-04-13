0540ccd5c0245826fb67dac583b5317a0e02259b
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-04-12 22:26:31 -0600

    Membership service is now calling into the running frame server to authenticate users as part of the oauth token grant.

--- Backend/ClientServices/auth/OAuthServiceModels/MembershipService.js
+++ Backend/ClientServices/auth/OAuthServiceModels/MembershipService.js
@@ -1,17 +1,27 @@
 // MembershipService.js
 var frame = require('frame');
+var frameLocalSvc = require('../frameLocalService');
 
 module.exports = MembershipService;
 
 function MembershipService()
 {
   console.log('MembershipService constructor');
+  frameLocalService = new frameLocalSvc();
 }
 
-MembershipService.prototype.areUserCredentialsValid = function(userName, password, scope, callback) {
-    // debug
-    return callback(null, true);
-
-    // todo:
-    // call into frame for credential verification
+MembershipService.prototype.areUserCredentialsValid = function(userName, password, scope, callback)
+{
+    frameLocalService.findByCredentials(userName, password, (err, user) => {
+      if (err)
+      {
+        console.error('Membership Service error: ' + err.messsage);
+        callback(err, false);
+      }
+      else
+      {
+        console.log('Membership Service: user creds are valid!');
+        callback(null, true);
+      }
+    });
 }
--- Backend/ClientServices/auth/frameLocalService.js
+++ Backend/ClientServices/auth/frameLocalService.js
@@ -2,14 +2,65 @@
 // this is an internal service API into the frame API for use during authentication
 // it abstracts the http requests to the running frame process
 
-var constants = require('../constants');
-var http = require('http');
+module.exports = FrameLocalService;
 
-module.exports = {
+const constants = require('../constants');
+const http = require('http');
+const request = require('request');
+const querystring = require('querystring');
 
-  // this method mirrors the method signature used by Frame
-  findByCredentials: function(username, password, (err, user)) {
-    // todo
+function FrameLocalService()
+{
+  frameAgent = new http.Agent({keepAlive: true});
+}
+
+FrameLocalService.prototype.findByCredentials = function(username, password, callback)
+{
+  // write the frame login request
+  const form = {
+    username: username,
+    password: password
   }
+
+  const formData = querystring.stringify(form);
+  const contentLength = formData.length;
+
+  const options = {
+    host: constants.USER_MANAGEMENT_INTERAL_API_URI,
+    port: constants.USER_MANAGEMENT_INTERNAL_API_PORT,
+    path: constants.USER_MANAGEMENT_INTERNAL_API_PATH_PREFIX + '/login',
+    method: 'POST',
+    agent: frameAgent,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': contentLength
+    }
+  }
+
+  const req = http.request(options, (res) => {
+    console.log('frame response status: ' + res.statusCode);
+    console.log('frame response headers: ' + JSON.stringify(res.headers));
+
+    res.on('data', (chunk) => {
+      console.log('frame response body chunk: ' + chunk);
+    });
+
+    res.on('end', () => {
+      console.log('frame response body end.');
+
+      if (res.statusCode != '200')
+        callback(new Error('did not work'), null);
+      else
+        callback(null, 'someuser');
+    });
+
+  });
+
+  req.on('error', (e) => {
+    console.error('problem with frame request: ' + e.message);
+  })
+
+  req.write(formData);
+  req.end();
 
 }
