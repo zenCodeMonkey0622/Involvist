5ebba44d16a587e53e0bfc958b3d31e9eff00e17
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-04-26 07:12:32 -0600

    Updated API documentation to include new user registration and bills by number.

--- Backend/ClientServices/endpoints/bills.js
+++ Backend/ClientServices/endpoints/bills.js
@@ -1,5 +1,5 @@
 //bills.js
-//The endpoints for the bills 
+//The endpoints for the bills
 'use strict'
 
 var express = require('express');
@@ -7,13 +7,13 @@ var billsRouter = express.Router();
 var billsService = require('../bills/BillsService');
 
 //Using .bind(billsService) so the callse will be in the scope of the billsService
-//Example of a search - http://localhost:3000/api/v1/bills/search?q=dog&client_id=1
+//Example of a search - http://localhost:3000/api/v1/bills/search?q=dog
 billsRouter.get('/', billsService.queryBills.bind(billsService), function(req, res) {
     res.json(req.bills);
 });
 
-//Example - http://localhost:3000/api/v1/bills/H.R.4881?client_id=1
-billsRouter.get('/:number', function(req, res){	
+//Example - http://localhost:3000/api/v1/bills/H.R.4881
+billsRouter.get('/:number', function(req, res){
     res.json(req.bills);
 });
 
