cd078674bb0c3bfcbeeb93dbeda5326c72df7d5d
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-06-02 09:06:49 -0600

    refactoring to have the sign-up flow through a page view controller.

---
+++ Mobile/InvolvistApp/InvolvistApp/SignIn/SignUpPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpPageViewController.swift
+//  InvolvistApp
+//
+//  Created by Ruben Hansen-Rojas on 6/2/17.
+//  Copyright © 2017 Involvist. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUpPageViewController: UIPageViewController
+{
+    
+}
