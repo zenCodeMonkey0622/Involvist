959dacf7a6d7d46b0a2941a9fb5ab7b938cce40a
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-06-26 22:02:14 -0600

    launching the authentication and gateway server on a secure connection. tested with postman and it works.

--- Backend/ClientServices/AuthStart.js
+++ Backend/ClientServices/AuthStart.js
@@ -5,6 +5,8 @@ const auth = require('./auth/authentication');
 const gateway = require('./endpoints/gateway');
 const express = require('express');
 const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 var app = express();
@@ -15,7 +17,12 @@ app.use('/oauth', auth.AuthenticationRouter);
 // todo: launch the api gateway on its own child process? 
 app.use('/api', gateway);
 
-// create the authentication http server
-http.createServer(app).listen(3000, () => {
-    console.log('rouser authentication server listening on port 3000');
+// create the authentication https server with ssl options
+var serverOptions = {
+    key: fs.readFileSync('ssl/server.key'),
+    cert: fs.readFileSync('ssl/server.crt')
+};
+
+https.createServer(serverOptions, app).listen(3443, () => {
+    console.log('rousr secure authentication and gateway server listening on port 3443');
 });
