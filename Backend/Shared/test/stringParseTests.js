5ddf32f81fc3adf404dd380ff2f7d9b18fb8256b
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-07-06 09:05:34 -0600

    Adding initial tests for parsePrimarySubjects.

--- Backend/Shared/test/stringParseTests.js
+++ Backend/Shared/test/stringParseTests.js
@@ -3,13 +3,24 @@
 'use strict';
 
 const testParser = require('../Parsers/stringParse');
+const assert = require('assert');
 
-var assert = require('assert');
+describe('stringParse', function() {
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+    describe('parsePrimarySubjects()', function() {
+
+        it('should return empty array when the value is null', function() {
+            var actualResult = testParser.parsePrimarySubjects(null);
+            assert.notEqual(actualResult, null);
+            assert.equal(actualResult.length, 0);
+        });
+
+        it('should return empty array when the value an empty array', function() {
+            var actualResult = testParser.parsePrimarySubjects([]);
+            assert.notEqual(actualResult, null);
+            assert.equal(actualResult.length, 0);
+        });
+
     });
-  });
+
 });
