80722ccf7ddda6128272355127edaf1cbb074d86
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-07-06 09:21:33 -0600

    more tests defined.

--- Backend/Shared/test/stringParseTests.js
+++ Backend/Shared/test/stringParseTests.js
@@ -15,10 +15,48 @@ describe('stringParse', function() {
             assert.equal(actualResult.length, 0);
         });
 
-        it('should return empty array when the value an empty array', function() {
-            var actualResult = testParser.parsePrimarySubjects([]);
+        it('should return empty array when the value an empty string', function() {
+            var actualResult = testParser.parsePrimarySubjects('');
             assert.notEqual(actualResult, null);
             assert.equal(actualResult.length, 0);
+        });
+
+        it('should return a single element when the value is a single string', function() {
+            var expectedSubject = 'Health';
+            var actualResult = testParser.parsePrimarySubjects(expectedSubject);
+            assert.notEqual(actualResult, null);
+            assert.equal(actualResult.length, 1);
+            assert.equal(actualResult[0], expectedSubject);
+        });
+
+        it('should return two elements when the value is a comma-delimited string', function() {
+            var expectedSubject = ['Health', 'Technology'];
+            var actualResult = testParser.parsePrimarySubjects('Health, Technology');
+            assert.notEqual(actualResult, null);
+            assert.equal(actualResult.length, 2);
+
+            expectedSubject.forEach( function(subject) {
+                assert.equal(actualResult[0], subject);
+            });
+        });
+
+        it('should return two elements when the value is a comma-delimited string with no spaces', function() {
+            var expectedSubject = ['Health', 'Technology'];
+            var actualResult = testParser.parsePrimarySubjects('Health,Technology');
+            assert.notEqual(actualResult, null);
+            assert.equal(actualResult.length, 2);
+
+            expectedSubject.forEach( function(subject) {
+                assert.equal(actualResult[0], subject);
+            });
+        });
+
+        it('should return one elements when the value is a space-delimited string', function() {
+            var expectedSubject = 'Media and Communications';
+            var actualResult = testParser.parsePrimarySubjects(expectedSubject);
+            assert.notEqual(actualResult, null);
+            assert.equal(actualResult.length, 1);
+            assert.equal(actualResult[0], expectedSubject);
         });
 
     });
