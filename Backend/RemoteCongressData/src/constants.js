a4e08722aa59e40c0918968364fa194542694fb4
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-07-18 20:37:56 -0600

    Adding comments to the pro publica api keys so we know to whom they are registered.

--- Backend/RemoteCongressData/src/constants.js
+++ Backend/RemoteCongressData/src/constants.js
@@ -2,11 +2,15 @@
 
 var constants = {};
 
+// todo: register propublica API key to Rousr, LLC
+// currently registered to r. crowley
 constants.PROPUBLICA_API_KEY = 'BQ03NR8CIK2paMdsiI2m05hBMiuGyyEY3jXJCLbp';
+// currently registered to r. hansen-rojas
 constants.PROPUBLICA_API_KEY_SECOND = 'd9MT6rGU1C9D5U1o32mMvkkeQPjUIZ4K4ZVplu60';
+
 constants.CONGRESS_API_HOST_URI = 'api.propublica.org';
 constants.BASE_CONGRESS_API_PATH = '/congress/v1';
-constants.BASE_CONGRESS_API_URI ='api.propublica.org/congress/v1';
+constants.BASE_CONGRESS_API_URI = 'api.propublica.org/congress/v1';
 constants.CURRENT_CONGRESS = sharedConstants.CURRENT_CONGRESS;
 constants.BILL_TYPES = ['introduced' , 'updated' , 'passed' , 'major'];
 constants.HOUSE_BILLS_URI = constants.BASE_CONGRESS_API_PATH + '/' + constants.CURRENT_CONGRESS + '/' + 'house/bills/';
