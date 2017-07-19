0af8bef40d0719b03eeb38e7a1db07528bf74015
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-07-18 22:46:36 -0600

    Initial skeleton for bill query by name tests.

---
+++ Backend/RousrAPI/test/billQueryByNameTests.js
@@ -0,0 +1,23 @@
+// billQueryByNameTests.js
+// unit testing suite for Rousr API bill query by name
+'use strict';
+
+const assert = require('assert');
+
+describe('Rousr API', function() {
+
+    describe('Bill Query By Name', function() {
+
+        it('should return invalid token error', function() {
+            assert.fail('unit test not implemented');
+        });
+
+        it('should return one bill matching the name', function() {
+            assert.fail('unit test not implemented');
+        });
+
+        it('should return zero bills', function() {
+            assert.fail('unit test not implemented');
+        });
+    });
+});
