7a22340e1aef7b99b4ed44b85680e5a2752738d4
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-08-06 15:18:49 -0600

    added before() and first 401 test to address to district tests. updated package.json to add a comment for how to run specific tests.

--- Backend/RousrAPI/test/testConfig.js
+++ Backend/RousrAPI/test/testConfig.js
@@ -9,11 +9,12 @@ module.exports = {
     TEST_AUTH_URI: 'localhost',
     TEST_AUTH_ENDPOINT: '/oauth/token',
     TEST_CLIENT_ID: '1',
-    TEST_CONGRESS_API_URI: 'localhost',
+    TEST_ROUSR_API_URI: 'localhost',
     TEST_CONGRESS_BILLS_ENDPOINT: '/api/v1/bills',
     TEST_CONGRESS_BILLS_QUERYNAME_PATH: '/name/',
     TEST_CONGRESS_BILLS_QUERYNUMBER_PATH: '/number/',
     TEST_CONGRESS_BILLS_QUERYSUBJECT_PATH: '/subject/',
+    TEST_ADDRESS_TO_DISTRICT_PATH: '/locations/congress/district/',
     TEST_USER: 'rhr@rousr.io',
     TEST_PASSWORD: 'feefifofum',
 
