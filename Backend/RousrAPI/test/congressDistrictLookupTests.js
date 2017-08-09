9c8a08cb41b20f52e6d83f827aec9173c14f90ea
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-08-09 00:16:02 -0600

    Added a few more congressional district lookup API integration tests.

--- Backend/RousrAPI/test/congressDistrictLookupTests.js
+++ Backend/RousrAPI/test/congressDistrictLookupTests.js
@@ -168,5 +168,161 @@ describe('Rousr API', function() {
                 queryRequest.end();
         });
 
+        it('should return TX district 35', function(done) {
+
+            // increase the timeout for this query
+            this.timeout(5000);
+
+            const expectedResponseCode = '200';
+            const expectedState = 'TX';
+            const expectedDistrict = 35;
+
+            const form = {
+                address: '115 Sandra Muraida Way, Austin, TX'
+            };
+        
+            const formData = querystring.stringify(form);
+
+            debugger;
+
+            const queryRequest = httpUtil.makeHttpsRequest(testConfig.TEST_ROUSR_API_URI,
+                sharedConfig.get('/gateway/svcPort'),
+                testConfig.TEST_ADDRESS_TO_DISTRICT_PATH,
+                httpUtil.requestType.GET,
+                secureAgent,
+                formData,
+                httpUtil.contentType.JSON,
+                {'Authorization': 'Bearer ' + testAuthToken},
+                testConfig.TEST_HTTP_OPTIONS,
+                (res) => {
+                    var responseData = '';
+
+                    res.on('data', (chunk) => {
+                        responseData += chunk;
+                    });
+
+                    res.on('end', () => {
+                        assert.equal(res.statusCode, expectedResponseCode, 'expected ' + expectedResponseCode + ' but received ' + res.statusCode);
+
+                        const responseObj = JSON.parse(responseData);
+
+                        if (responseObj == null) {
+                            assert.fail('deserialization failure');
+                            done();
+                        }
+                        
+                        assert.equal(responseObj.success, true, 'did not receive successful response');
+
+                        if (responseObj.data == null) {
+                            assert.fail('response data was null');
+                            done();
+                        }
+                        
+                        if (responseObj.data.geoCoordinate == null) {
+                            assert.fail('response data geoCoordinate was null');
+                            done();
+                        }
+
+                        if (responseObj.data.civicData == null) {
+                            assert.fail('response data civicData was null');
+                            done();
+                        }
+
+                        const actualState = responseObj.data.civicData.state;
+                        const actualDistrict = responseObj.data.civicData.districtNumber;
+
+                        assert.equal(actualState, expectedState, 'expected ' + expectedState + ' but received ' + actualState);
+                        assert.equal(actualDistrict, expectedDistrict, 'expected ' + expectedDistrict + ' but received ' + actualDistrict);
+
+                        done();
+                    });
+                });
+
+                queryRequest.on('error', (e) => {
+                    assert.fail(null, null, 'congress district lookup request: ' + e);
+                    done();
+                });
+
+                queryRequest.end();
+        });
+
+        it('should return AL district 5', function(done) {
+
+            // increase the timeout for this query
+            this.timeout(5000);
+
+            const expectedResponseCode = '200';
+            const expectedState = 'AL';
+            const expectedDistrict = 5;
+
+            const form = {
+                address: '155 Whisperwood Ln, Madison, AL'
+            };
+        
+            const formData = querystring.stringify(form);
+
+            debugger;
+
+            const queryRequest = httpUtil.makeHttpsRequest(testConfig.TEST_ROUSR_API_URI,
+                sharedConfig.get('/gateway/svcPort'),
+                testConfig.TEST_ADDRESS_TO_DISTRICT_PATH,
+                httpUtil.requestType.GET,
+                secureAgent,
+                formData,
+                httpUtil.contentType.JSON,
+                {'Authorization': 'Bearer ' + testAuthToken},
+                testConfig.TEST_HTTP_OPTIONS,
+                (res) => {
+                    var responseData = '';
+
+                    res.on('data', (chunk) => {
+                        responseData += chunk;
+                    });
+
+                    res.on('end', () => {
+                        assert.equal(res.statusCode, expectedResponseCode, 'expected ' + expectedResponseCode + ' but received ' + res.statusCode);
+
+                        const responseObj = JSON.parse(responseData);
+
+                        if (responseObj == null) {
+                            assert.fail('deserialization failure');
+                            done();
+                        }
+                        
+                        assert.equal(responseObj.success, true, 'did not receive successful response');
+
+                        if (responseObj.data == null) {
+                            assert.fail('response data was null');
+                            done();
+                        }
+                        
+                        if (responseObj.data.geoCoordinate == null) {
+                            assert.fail('response data geoCoordinate was null');
+                            done();
+                        }
+
+                        if (responseObj.data.civicData == null) {
+                            assert.fail('response data civicData was null');
+                            done();
+                        }
+
+                        const actualState = responseObj.data.civicData.state;
+                        const actualDistrict = responseObj.data.civicData.districtNumber;
+
+                        assert.equal(actualState, expectedState, 'expected ' + expectedState + ' but received ' + actualState);
+                        assert.equal(actualDistrict, expectedDistrict, 'expected ' + expectedDistrict + ' but received ' + actualDistrict);
+
+                        done();
+                    });
+                });
+
+                queryRequest.on('error', (e) => {
+                    assert.fail(null, null, 'congress district lookup request: ' + e);
+                    done();
+                });
+
+                queryRequest.end();
+        });
+
     });
 });
