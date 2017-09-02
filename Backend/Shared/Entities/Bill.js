657b188e9ef958687f67d09bcecee114620decf0
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-09-01 23:23:42 -0600

    Moving our models into an Entities folder.

---
+++ Backend/Shared/Entities/Bill.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const sharedConfig = require('../Config/SharedConfig');
+
+// we use mongoose for mongoDb object modeling
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Information from get Bills by congressman and specific details
+var BillSchema = new Schema({
+	congress: String,
+	number: String,
+	bill: String,
+	bill_uri: String,
+	title: String,
+	sponsor: String,
+	sponsor_id: String,
+	sponsor_uri: String,
+	sponsor_party: String,
+    sponsor_state: String,
+    gpo_pdf_uri: String,
+    congressdotgov_url: String,
+    govtrack_url: String,
+    introduced_date: String,
+    active: String,
+    house_passage: String,
+    senate_passage: String,
+    enacted: String,
+    vetoed: String,    
+	cosponsors: String,
+	primary_subject: String,
+	committees: String,    
+	latest_major_action_date: String,
+	latest_major_action: String,
+	last_vote_date: String,
+	house_passage_vote: String,
+	senate_passage_vote: String,
+	summary: String,
+    summary_short: String,
+	versions: [],
+	actions: [{}],
+	//Added 
+	primary_subjects: [],
+	tags: [],
+	description: String,
+    name: String
+});
+
+var Bill = mongoose.model("rsr_current_bill_" + sharedConfig.get('/currentCongress'), BillSchema);
+
+module.exports.Bill = Bill;
+
+
---
+++ Backend/Shared/Entities/RousrUser.js
@@ -0,0 +1,31 @@
+﻿// RousrUser.js
+// defines the Rousr User model
+
+'use strict'
+
+// we use mongoose for mongoDb object modeling
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Create our own user from the FramUser data
+var RousrUserSchema = new Schema({
+    userID: String,
+    userName: String,
+    realName: String,
+    email: String,    
+    city: String,
+    state: String,
+    zipCode: String,
+    followingBills : []
+});
+
+RousrUserSchema.methods.initialize = function (userName, realName, email, userID) {
+    this.userID = userID;
+    this.userName = userName;
+    this.realName = realName;
+    this.email = email;
+}
+
+var rousrUser = mongoose.model("rsr_user", RousrUserSchema);
+module.exports.RousrUser = rousrUser;
+
