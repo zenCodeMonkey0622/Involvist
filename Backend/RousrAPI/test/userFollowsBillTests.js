46c8ce64304577c8eb5600aa1e3672a5b933bc57
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-09-02 23:21:50 -0600

    added unit test starters for user follows bill API.

---
+++ Backend/RousrAPI/test/userFollowsBillTests.js
@@ -0,0 +1,77 @@
+// userFollowsBillTests.js
+// unit tests for user following bills api
+
+'use strict';
+
+const env = require('./testEnv').setTestEnv();
+
+const https = require('https');	
+const assert = require('assert');
+const querystring = require('querystring');
+const testConfig = require('./testConfig');
+const testHelpers = require('./apiTestHelpers');
+const httpUtil = require('../../Shared/ServiceAccess/httpUtility');
+const sharedConfig = require('../../Shared/Config/SharedConfig');
+
+const secureAgent = new https.Agent({keepAlive: true});
+
+var testAuthToken = '';
+
+/**
+ * before() - runs code before all tests are loaded
+ * * @param {function()} done - callback executed when async code is complete.
+ */
+before( function(done) {
+    
+        // increase the timeout for this query
+        this.timeout(5000);
+    
+        testHelpers.getAuthToken(testConfig.TEST_USER,
+            testConfig.TEST_PASSWORD,
+            (err, token) => {
+                if (err) {
+                    assert.fail('unable to get authentication token');
+                }
+                else {
+                    testAuthToken = token;
+                }
+                done();
+            });
+    });
+
+/**
+ * after() - runs code after all tests are run
+ */
+after( function() {
+    // close all open sockets used by this agent
+    secureAgent.destroy();
+});
+
+describe('Rousr API', function() {
+    
+        describe('User Follows Bill', function() {
+    
+            // 'done' parameter used on async code. call done() after
+            // last assert so mocha knows it needs to wait before executing
+            // other tests
+            it('should return 401 Unauthorized', function(done) {
+                assert.fail(null, null, 'unit test not implemented');
+            });
+
+            it('should add bill id to user object', function(done) {
+                assert.fail(null, null, 'unit test not implemented');
+            });
+
+            it('should not add bill id again to user object', function(done) {
+                assert.fail(null, null, 'unit test not implemented');
+            });
+
+            it('should create a relationship meta object', function(done) {
+                assert.fail(null, null, 'unit test not implemented');
+            });
+
+            it('should not create more than one relationship meta object', function(done) {
+                assert.fail(null, null, 'unit test not implemented');
+            });
+        });
+    });
