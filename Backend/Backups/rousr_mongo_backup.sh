6470a3ca74ae1e25546c02d9995cbc96e7ae71ae
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2017-09-03 22:10:20 -0600

    updated the backup script to take in an argument for the environments. either dev or test. also copies backups to test or dev directories.

--- Backend/Backups/rousr_mongo_backup.sh
+++ Backend/Backups/rousr_mongo_backup.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
 today=$(date +%F)
+rsrEnv=$1
 
-mkdir $today
-mongodump -h ds151433.mlab.com:51433 -d rsrtest -u root -p traficand0rimas -o "rsrtest_mongo_backup"
-cp -r "rsrtest_mongo_backup" "$today"
+if ! [ -d $1 ]; then
+    mkdir $1
+fi
+
+if ! [ -d $1/$today ]; then
+    mkdir $1/$today
+fi
+
+if [ $1 = "test" ]; then
+    host="ds151433.mlab.com:51433"
+    authDb="rsrtest"
+    user="root"
+    pw="traficand0rimas"
+elif [ $1 = "dev" ]; then
+    host="ds147069.mlab.com:47069"
+    authDb="projectwoke"
+    user="root"
+    pw="g0ld0ntheceiling"
+else
+    echo "invalid parameters. must be either test or dev"
+    exit -1
+fi
+
+mongodump -h $host -d $authDb -u $user -p $pw -o "rsr_mongo_backup"
+cp -r "rsr_mongo_backup" "$1/$today"
+mv "rsr_mongo_backup" "$1"
 
